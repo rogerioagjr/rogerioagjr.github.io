3f2588ea2314b5ef7f1b1b04733228a9fae7ec9d
rogerioagjr <rogerio@caltech.edu>
2026-07-24 13:11:06 -0700

    fixed external links script

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -1,11 +1,35 @@
-// Opens all external (absolute URL) links in a new tab. Internal site links
-// always use relative paths, so any anchor pointing to an absolute http(s)
-// URL is treated as external.
+// Opens external links (any domain other than this site's own domains) in a
+// new tab. The CV link is always opened in a new tab too, even though it's
+// hosted on the same domain, since it's a PDF file rather than a site page.
 (function () {
+  var internalHosts = [
+    'rogeriojr.com',
+    'www.rogeriojr.com',
+    'rogerioagjr.github.io',
+    'localhost',
+    '127.0.0.1'
+  ];
+
+  function isCvLink(pathname) {
+    return /\/files\/CV\.pdf$/i.test(pathname);
+  }
+
   function markExternalLinks() {
     var anchors = document.querySelectorAll('a[href^="http://"], a[href^="https://"]');
 
     anchors.forEach(function (link) {
+      var url;
+      try {
+        url = new URL(link.href);
+      } catch (e) {
+        return;
+      }
+
+      var isInternal = internalHosts.indexOf(url.hostname) !== -1;
+      if (isInternal && !isCvLink(url.pathname)) {
+        return;
+      }
+
       link.setAttribute('target', '_blank');
 
       var relValues = (link.getAttribute('rel') || '').split(' ').filter(Boolean);
